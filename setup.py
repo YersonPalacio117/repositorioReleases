16d04fdce828c268b10c50843bf363121b459327
Yerson Palacio <ympalacio@celerix.com>
2025-07-11 08:56:12 -0500

    Lanzamiento de paquete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repositorioReleases",                     # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Yerson Palacio",                        # Tu nombre
+    author_email="ympalacio@celerix.com",           # Tu correo electrónico
+    url="https://github.com/YersonPalacio117/repositorioReleases#",     # URL del proyecto
+)
